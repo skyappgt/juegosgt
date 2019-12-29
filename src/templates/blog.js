7b86b0305deab9e8659f227aa690890e1ff31daf
Romeosky <skyordive@gmail.com>
2019-12-28 21:16:24 -0600

    fix admin cotiza

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -39,7 +39,7 @@ export default class BlogPage extends Component {
     return (
       <Layout>
         <Helmet>
-          <title>Blog | Gatsby Starter Business</title>
+          <title>Cotizaciones</title>
           {/* Schema.org tags */}
           <script type='application/ld+json'>
             {JSON.stringify(websiteSchemaOrgJSONLD)}
