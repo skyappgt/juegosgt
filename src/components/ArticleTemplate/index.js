1e6127cafa4ed665902eec00e0a65f4ceb7d31c3
skyappgt <itsielec@gmail.com>
2020-01-24 19:46:43 -0600

    logo display

--- src/components/ArticleTemplate/index.js
+++ src/components/ArticleTemplate/index.js
@@ -16,7 +16,9 @@ const ArticleTemplate = ({
   const PostContent = contentComponent || Content
   return (
     <div>
-      <h2> Jueg</h2><img  className="logo" src={logo} alt={title} />
+      <div className="title is-size-2 has-text-weight-bold">
+        <h2> Jueg</h2><img  className="logo" src={logo} alt={title} />
+      </div>
       <h1 className='title is-size-2 has-text-weight-bold is-bold-light'>
         {title}
       </h1>
