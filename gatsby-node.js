5992aca13161886b91afbcef58ea407e540ab1ed
Romeosky <skyordive@gmail.com>
2022-03-23 09:58:12 -0600

    gatsbynode, toml, wooden.todler

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,36 +1,7 @@
 const _ = require('lodash')
 const path = require('path')
+const { createFilePath } = require('gatsby-source-filesystem')
 const createPaginatedPages = require('gatsby-paginate')
-
-exports.onCreateNode = ({ node, actions, getNode }) => {
-  const { createNodeField } = actions
-  let slug
-
-  if (node.internal.type === `MarkdownRemark`) {
-    const fileNode = getNode(node.parent)
-    const parsedFilePath = path.parse(fileNode.relativePath)
-
-    if (Object.prototype.hasOwnProperty.call(node, 'frontmatter')) {
-      if (Object.prototype.hasOwnProperty.call(node.frontmatter, 'slug') && Object.prototype.hasOwnProperty.call(node.frontmatter, 'cover')) {
-        slug = `/blog/${_.kebabCase(node.frontmatter.slug)}`
-      } else if (Object.prototype.hasOwnProperty.call(node.frontmatter, 'slug')) {
-        slug = `/${_.kebabCase(node.frontmatter.slug)}`
-      } else if (parsedFilePath.name !== 'index' && parsedFilePath.dir !== '') {
-        slug = `/${parsedFilePath.dir}/${parsedFilePath.name}/`
-      } else if (parsedFilePath.dir === '') {
-        slug = `/`
-      } else {
-        slug = `/${parsedFilePath.dir}/`
-      }
-    }
-
-    createNodeField({
-      name: `slug`,
-      node,
-      value: slug,
-    })
-  }
-}
 
 exports.createPages = ({ actions, graphql }) => {
   const { createPage } = actions
@@ -47,18 +18,7 @@ exports.createPages = ({ actions, graphql }) => {
             }
             frontmatter {
               title
-              cover {
-                childImageSharp{
-                  fluid (maxWidth:500, quality:50){
-                    src
-                    srcSet
-                    aspectRatio
-                    sizes
-                    base64
-                  }
-                }
-                publicURL
-              }
+              cover
               tags
               templateKey
               date(formatString: "MMMM DD, YYYY")
@@ -89,7 +49,7 @@ exports.createPages = ({ actions, graphql }) => {
       createPage: createPage,
       pageTemplate: 'src/templates/blog.js',
       pageLength: 6, // This is optional and defaults to 10 if not used
-      pathPrefix: 'blog', // This is optional and defaults to an empty string if not used
+      pathPrefix: 'cotiza', // This is optional and defaults to an empty string if not used
       context: {}, // This is optional and defaults to an empty object if not used
     })
     postsAndPages.forEach(edge => {
@@ -98,7 +58,7 @@ exports.createPages = ({ actions, graphql }) => {
         path: edge.node.fields.slug,
         tags: edge.node.frontmatter.tags,
         component: path.resolve(
-          `src/templates/${String(edge.node.frontmatter.templateKey)}.js`,
+          `src/templates/${String(edge.node.frontmatter.templateKey)}.js`
         ),
         // additional data can be passed via context
         context: {
@@ -131,4 +91,17 @@ exports.createPages = ({ actions, graphql }) => {
       })
     })
   })
+}
+
+exports.onCreateNode = ({ node, actions, getNode }) => {
+  const { createNodeField } = actions
+
+  if (node.internal.type === `MarkdownRemark`) {
+    const value = createFilePath({ node, getNode })
+    createNodeField({
+      name: `slug`,
+      node,
+      value,
+    })
+  }
 }
